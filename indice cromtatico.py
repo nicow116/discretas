82efa42b7725316428d837bbcf93597c5cb9b4ac
Lepommejamez <98928651+Lepommejamez@users.noreply.github.com>
2023-04-21 00:23:43 -0500

    SIUUU

--- indice cromtatico.py
+++ indice cromtatico.py
@@ -1,22 +1,38 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Definimos el grafo de ejemplo
-G = nx.Graph()
-G.add_edges_from([(1,2),(1,3),(2,3),(2,4),(3,4),(4,5),(4,6),(5,6)])
+# Creamos un grafo aleatorio con 8 nodos y 12 aristas
+G = nx.gnm_random_graph(6, 6)
 
-# Calculamos el índice cromático del grafo
-chrom_index = nx.chromatic_index(G)
+# Creamos una lista de colores para colorear las aristas
+colors = ['r', 'g', 'b', 'c', 'm', 'y']
 
-# Asignamos diferentes colores a los nodos de cada conjunto en la partición del índice cromático
-node_colors = {}
-for i, color_set in enumerate(nx.coloring.greedy_color(G, strategy='largest_first').values()):
-    for node in color_set:
-        node_colors[node] = i
+# Creamos una lista para llevar el seguimiento de los colores utilizados
+used_colors = []
 
-# Dibujamos el grafo con diferentes colores para cada conjunto
-nx.draw(G, with_labels=True, node_color=[node_colors[node] for node in G.nodes()])
+# Recorremos todas las aristas del grafo
+for u, v in G.edges():
 
-# Mostramos el grafo y el índice cromático en la consola
+    # Obtenemos los colores asignados a las aristas adyacentes a u y v
+    adjacent_colors_u = [G[u][adj]['color'] for adj in G.neighbors(u) if 'color' in G[u][adj]]
+    adjacent_colors_v = [G[v][adj]['color'] for adj in G.neighbors(v) if 'color' in G[v][adj]]
+
+    # Creamos un conjunto de colores adyacentes a las aristas de u y v
+    adjacent_colors = set(adjacent_colors_u + adjacent_colors_v)
+
+    # Buscamos el primer color de los utilizados que no está en el conjunto de colores adyacentes
+    for color in used_colors:
+        if color not in adjacent_colors:
+            break
+    else:
+        # Si no encontramos ningún color utilizado que no esté en el conjunto de colores adyacentes, tomamos uno de la lista de colores
+        color = colors[len(used_colors) % len(colors)]
+        used_colors.append(color)
+
+    # Asignamos el color encontrado a la arista (u, v)
+    G[u][v]['color'] = color
+
+# Dibujamos el grafo con las aristas coloreadas
+nx.draw(G, with_labels=True, edge_color=[d['color'] for u, v, d in G.edges(data=True)])
+print("Indice cromatico:"+str(len(used_colors)))
 plt.show()
-print("El índice cromático del grafo es:", chrom_index)
